448fe8a03da25dbd051ab91c8078a52b08d747c3
Coops530 <cooper530@users.noreply.github.com>
2019-03-25 19:10:51 -0400

    HUGE UPDATE
    Did a large basis on first few sections. Decided to convert to mouse clicks as the Disney website uses javascript for buttons, not html. Finally, began working on user input for months and days, as that way you can determine the calander day

--- test.py
+++ test.py
@@ -1,35 +1,3 @@
-from twilio.rest import Client
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-import json
-import os
-import threading
-import time as TIME
+import calendar
 
-driver = webdriver.Chrome()
-#What website to access
-driver.get("http://www.python.org")
-
-#Confirms website is correct
-assert "Python" in driver.title
-#Finds elements by key
-#elem = driver.find_element_by_name("q")
-elem = driver.find_element_by_id("submit")
-elem.click()
-time.sleep(3)
-'''
-#Searches by certain keys: "pycon" and RETURN key
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-
-#Checks if nothing was really found
-assert "No results found." not in driver.page_source
-'''
-#Closes browser
-driver.close()
+print (calendar.monthcalendar(2019,4))
--- test2.py
+++ test2.py
@@ -1,16 +1,61 @@
 import time
+import pyautogui
+import keyboard
+import calendar
 from selenium import webdriver
+import datetime
 
-password = ""
+d = datetime.date.today()
+current_month = d.month
+password = "mmc4four"
+
+'''
+1 = Monday
+2 = Tuesday
+3 = Wednesday
+4 = Thursday
+5 = Friday
+6 = Saturday
+7 = Sunday
+'''
+
+#INPUTS
+month = int(input("Enter desired month (numerical form): "))
+day = int(input("Enter desired day (numerical form): "))
+month_click = month - current_month
+
+#Finds what day of the week the day is
+week_day = datetime.date(2019, month, day).isoweekday()
+
+#Finds what week the day is in (Week 0,1,2,etc.) CAN BE LIST FORM
+print (calendar.monthcalendar(2019, month))
+
+
 driver = webdriver.Chrome()
 #What website to access
 driver.get("https://disneyworld.disney.go.com/fastpass-plus/select-party/")
-
-#Confirms website is correct
-assert "Fastpass+ Planning" in driver.title
+time.sleep(2)
 #Finds elements by key
-elem = driver.find_element_by_id("submit")
-elem.click()
+username = driver.find_element_by_name("username")
+pswd = driver.find_element_by_name("password")
+submit = driver.find_element_by_name("submit")
+#Enter the keys
+username.send_keys("mmc.4@comcast.net")
+pswd.send_keys(password)
+submit.click()
+time.sleep(5)
+#Select Party Page
+pyautogui.click(152, 707)
+time.sleep(.5)
+pyautogui.click(1190, 975)
+time.sleep(2)
+#Month Screen
+for i in range(month_click):
+    pyautogui.click(821, 352)
 time.sleep(3)
+if week_day == 1:
+    pyautogui.click(315, 480)
+
+print (pyautogui.position())
 
 driver.close()
