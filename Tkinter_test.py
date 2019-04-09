3306cbb808d5fa6434b12db15e0c9b177f74ebfe
Coops530 <cooper530@users.noreply.github.com>
2019-04-09 11:30:35 -0400

    Update Tkinter_test.py

--- Tkinter_test.py
+++ Tkinter_test.py
@@ -1,8 +1,14 @@
 import tkinter
 
+def submitData():
+    print("Information submitted!")
+
 root = tkinter.Tk()
 root.title("Fastpass+ Selection")
 root.geometry("500x500")
+submit = tkinter.Button(root, text = "Submit", height=1,width=10, command=submitData)
+
+submit.place(x=400,y=470)
 # Code to add widgets will go here...
 root.mainloop()
 
