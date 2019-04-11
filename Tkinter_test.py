2b2522fa32ea641349078123c5e5fee7e6ae2ba8
Coops530 <cooper530@users.noreply.github.com>
2019-04-11 11:17:09 -0400

    Update Tkinter_test.py

--- Tkinter_test.py
+++ Tkinter_test.py
@@ -1,12 +1,19 @@
 import tkinter
 parkFinal = ""
+monthFinal = ""
+
 def submitData():
     print("Information submitted!")
     print(str(parkFinal))
+    print(str(monthFinal))
     quit()
 def getPark(value):
     global parkFinal
     parkFinal = value
+def getMonth(value):
+    global monthFinal
+    monthFinal = value
+
 
 root = tkinter.Tk()
 root.title("Fastpass+ Selection")
@@ -24,10 +31,13 @@ parkDrop.insert(4, "Animal Kingdom")
 parkShow = tkinter.StringVar(root)
 parkShow.set("Park Selection")
 park = tkinter.OptionMenu(root, parkShow, "mk", "epcot", "hws", "ak", command=getPark)
-
+monthShow = tkinter.StringVar(root)
+monthShow.set("Month Selection")
+month = tkinter.OptionMenu(root, monthShow, "March", "April", "May", command=getMonth)
 #Place the widgets
 submit.place(rely=1.0, relx=1.0, x=0, y=0, anchor="se")
 park.place(x=10.0,y=10.0)
+month.place(x=130.0,y=10.0)
 root.mainloop()
 
 
