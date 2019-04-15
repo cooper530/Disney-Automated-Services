1a3996985fcb697767cffcb4dfa299fbed588a86
Coops530 <cooper530@users.noreply.github.com>
2019-04-15 09:44:40 -0400

    Update AppiumTests.py

--- AppiumTests.py
+++ AppiumTests.py
@@ -1,12 +1,13 @@
 # iOS environment
 import unittest
 from appium import webdriver
+import os
 
 desired_caps = {}
 desired_caps['platformName'] = 'iOS'
 desired_caps['platformVersion'] = '11.4'
 desired_caps['automationName'] = 'xcuitest'
 desired_caps['deviceName'] = 'iPhone Simulator'
-desired_caps['app'] = PATH('../../apps/UICatalog.app.zip')
+desired_caps['app'] = os.path.abspath(r'C:\Users\Cooper\Desktop\DisneyWorld\Payload\WDW.app')
 
 self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
