502acd99845ae5b0e9ed81695294edb7ecdf2d8c
Coops530 <cooper530@users.noreply.github.com>
2019-04-24 11:24:29 -0400

    Update DVC_Resale_Checker.py

--- DVC_Resale_Checker.py
+++ DVC_Resale_Checker.py
@@ -2,13 +2,12 @@ from selenium import webdriver
 from selenium import common
 import time
 
-
+resortDict = {"copper creek": "copper-creek-villas-cabins-disneys-wilderness-lodge"}
 resort = input("Enter home resort name: ")
 
-if (resort == "copper creek"):
-    resort = "copper-creek-villas-cabins-disneys-wilderness-lodge"
+resort = resortDict.get(resort)
 
 driver = webdriver.Chrome()
-#
+
 driver.get("https://www.fidelityresales.com/resort/" + resort)
-driver.find_element_by_class_name()
+#driver.find_element_by_class_name()
